fe3657197cd9778555cfe6b680261011d178d032
Gabriel Rascovit <rascovit@gmail.com>
2020-11-16 21:50:12 -0300

    create user model

---
+++ db/migrate/20201116031015_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone_number
+      t.string :avatar_url
+      t.string :provider
+      t.string :uid
+
+      t.timestamps
+    end
+  end
+end
